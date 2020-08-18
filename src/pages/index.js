11c115cb9ff94e0d29d0680061046ae2159a8f42
Josh Guha <joshguha@gmail.com>
2020-08-18 14:23:58 +0100

    Init commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import React from "react"
+import Layout from '../components/layout'
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+      <h1>Home page</h1>
+    </Layout>
+  )
 }
