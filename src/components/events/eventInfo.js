df7c2ed5b671b2bfac4dc96b88abcd7c7726eef8
Josh Guha <joshguha@gmail.com>
2020-09-03 13:44:58 +0100

    Event Info Made Responsive To Menu Selection

--- src/components/events/eventInfo.js
+++ src/components/events/eventInfo.js
@@ -4,6 +4,42 @@ import eventInfoStyles from "../../styles/components/events/eventInfo.module.scs
 export default function EventInfo() {
     const [selected, setSelected] = useState("weekly")
 
+    const renderBackground = () => {
+        switch (selected) {
+            case "weekly":
+                return `${eventInfoStyles.contentContainer} ${eventInfoStyles.weekly} `
+            case "flagship":
+                return `${eventInfoStyles.contentContainer} ${eventInfoStyles.flagship}`
+            case "retreats":
+                return `${eventInfoStyles.contentContainer} ${eventInfoStyles.retreats}`
+            default:
+                return eventInfoStyles.transparent
+        }
+    }
+
+    const renderContent = () => {
+        switch (selected) {
+            case "weekly":
+                return (
+                    <p>
+                        Our weekly events provide a place for students and
+                        faculty members alike to connect to themselves and other
+                        like minded individuals in a safe, light hearted
+                        environment where they can detach themselves from any
+                        external worries and stress and learn more about ways in
+                        which they can live a more driven and fulfilling
+                        lifestyle.
+                    </p>
+                )
+            case "flagship":
+                return <p>Information on Flagship events go here</p>
+            case "retreats":
+                return <p>Information on Retreats!</p>
+            default:
+                return "Error invalid 'selected' variable"
+        }
+    }
+
     return (
         <div className={eventInfoStyles.outerContainer}>
             <div className={eventInfoStyles.menu}>
@@ -45,7 +81,9 @@ export default function EventInfo() {
                 </div>
             </div>
 
-            <div className={eventInfoStyles.contentContainer}></div>
+            <div className={renderBackground()}>
+                <div className={eventInfoStyles.content}>{renderContent()}</div>
+            </div>
         </div>
     )
 }
