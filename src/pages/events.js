a4e43781e9dcb7c78524cc740f96b273724e3684
Josh Guha <joshguha@gmail.com>
2020-09-03 16:07:34 +0100

    Added Events Calendar

--- src/pages/events.js
+++ src/pages/events.js
@@ -3,6 +3,7 @@ import Header from "../components/header"
 import FadeIn from "../components/fadeIn"
 import Fade from "react-reveal/Fade"
 import EventInfo from "../components/events/eventInfo"
+import EventCalendar from "../components/events/eventCalendar"
 
 export default function Events() {
     return (
@@ -10,6 +11,9 @@ export default function Events() {
             <Header isOpaque={true} dark={true} />
             <Fade>
                 <EventInfo />
+            </Fade>
+            <Fade>
+                <EventCalendar />
             </Fade>
         </FadeIn>
     )
