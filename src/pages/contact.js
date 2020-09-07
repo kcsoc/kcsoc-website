9535116946a47438887f6124d63cd8f754b66346
Josh Guha <joshguha@gmail.com>
2020-09-07 18:21:18 +0100

    Font Awesome Icons Added and Centered

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,12 +2,10 @@ import React from "react"
 import Header from "../components/header"
 import FadeIn from "../components/fadeIn"
 import ContactInfo from "../components/contact/contactInfo"
-import { FaPhone } from 'react-icons/fa';
 
 export default function Contact() {
     return (
         <div>
-            <FaPhone />
             <FadeIn>
                 <Header isOpaque={true} dark={true} />
                 <ContactInfo />
