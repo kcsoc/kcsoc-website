05ec02626b429d1c41850aa10c7f08e7eec5891d
Dhaval Soneji <dhaval@soneji.xyz>
2020-09-07 13:52:14 +0100

    Revert "Event Menu Components Added"
    This reverts commit 1558d910ec6519f9ec3d8352fe01eaba03d2bcfc.

--- src/components/events/eventCalendar.js
+++ src/components/events/eventCalendar.js
@@ -1,12 +1,15 @@
-import React, { useState } from "react"
-import CalendarMenu from "./calendarMenu"
+import React from "react"
+
 import eventCalendarStyles from "../../styles/components/events/eventCalendar.module.scss"
 
 export default function EventCalendar() {
     return (
         <div className={eventCalendarStyles.outerContainer}>
             <h1>Calendar</h1>
-            <CalendarMenu />
+
+            <div className={eventCalendarStyles.menuContainer}>
+                <h2>i3ji</h2>
+            </div>
         </div>
     )
 }
