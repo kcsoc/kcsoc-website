fc2f1bc4a7083091812e04b120d2d95021492d74
Dhaval Soneji <dhaval@soneji.xyz>
2020-09-07 13:52:15 +0100

    Revert "Added Events Calendar"
    This reverts commit a4e43781e9dcb7c78524cc740f96b273724e3684.

--- src/pages/events.js
+++ src/pages/events.js
@@ -3,7 +3,6 @@ import Header from "../components/header"
 import FadeIn from "../components/fadeIn"
 import Fade from "react-reveal/Fade"
 import EventInfo from "../components/events/eventInfo"
-import EventCalendar from "../components/events/eventCalendar"
 
 export default function Events() {
     return (
@@ -11,9 +10,6 @@ export default function Events() {
             <Header isOpaque={true} dark={true} />
             <Fade>
                 <EventInfo />
-            </Fade>
-            <Fade>
-                <EventCalendar />
             </Fade>
         </FadeIn>
     )
