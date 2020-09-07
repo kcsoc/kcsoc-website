2a56d8cabba853d29420fe1e29e830ef13277fef
Josh Guha <joshguha@gmail.com>
2020-09-07 13:12:05 +0100

    Revert "Changes made to Gatsby Config and Node files"
    This reverts commit 35269e9d62d657b019c6c1730c5db840870a3289.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,42 +1,7 @@
-require("dotenv").config({ path: __dirname + "/.env" })
-
 module.exports = {
     siteMetadata: {
-        title: "KCSOC",
-        author: "KCSOC National",
+        title: "KCSoc",
+        author: "KCSoc National",
     },
-    plugins: [
-        "gatsby-plugin-react-helmet",
-        {
-            resolve: "gatsby-source-contentful",
-            options: {
-                spaceId: process.env.CONTENTFUL_SPACE_ID,
-                accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-            },
-        },
-        "gatsby-plugin-sass",
-        {
-            resolve: "gatsby-source-filesystem",
-            options: {
-                name: "src",
-                path: `${__dirname}/src/`,
-            },
-        },
-        "gatsby-plugin-sharp",
-        {
-            resolve: "gatsby-transformer-remark",
-            options: {
-                plugins: [
-                    "gatsby-remark-relative-images",
-                    {
-                        resolve: "gatsby-remark-images",
-                        options: {
-                            maxWidth: "750",
-                            linkImagesToOriginal: false,
-                        },
-                    },
-                ],
-            },
-        },
-    ],
+    plugins: ["gatsby-plugin-sass", "gatsby-plugin-react-helmet"],
 }
