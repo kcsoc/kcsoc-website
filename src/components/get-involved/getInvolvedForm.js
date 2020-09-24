aaf09433efe70e3f2e18eadfa50ffb98d6181157
Josh Guha <joshguha@gmail.com>
2020-09-24 17:54:34 +0100

    Added http request to Get Involved form and form submission message

--- src/components/get-involved/getInvolvedForm.js
+++ src/components/get-involved/getInvolvedForm.js
@@ -1,25 +1,39 @@
-import React from "react"
+import React, { useState } from "react"
 import { useForm } from "react-hook-form"
-import axios from "axios"
 import formStyles from "../../styles/form.module.scss"
 
 export default function GetInvolvedForm() {
-    const url =
-        "https://script.google.com/macros/s/AKfycbxlISPgPe8OHWg9yMtBqQv3PjXAf_iDVF0wfieKhsas_LN1cR0/exec"
-    const { register, handleSubmit, errors } = useForm()
-    const onSubmit = data => {
+    const [disabledButton, setDisabledButton] = useState(false)
+    const [message, setMessage] = useState("")
+
+    const { register, handleSubmit, errors, reset } = useForm()
+
+    // CHANGE FOR PRODUCTION //////////////
+    const url = "http://localhost:5000/"
+    //////////////////////////////////////
+
+    const onSubmit = async data => {
         try {
-            axios.post(url, {
+            setDisabledButton(true)
+            setMessage("")
+            const res = await fetch(`${url}forms/get-involved`, {
+                method: "POST",
                 headers: {
                     "Content-Type": "application/json",
-                    "Access-Control-Allow-Origin": "*",
                 },
-                data,
+                body: JSON.stringify(data),
             })
+            reset()
+            if (res.status === 201) {
+                setMessage("Form submitted successfully")
+            }
+
+            setDisabledButton(false)
         } catch (e) {
+            setDisabledButton(false)
+
             console.log(e)
         }
-        console.log(data)
     }
 
     return (
@@ -27,15 +41,27 @@ export default function GetInvolvedForm() {
             <form className={formStyles.form} onSubmit={handleSubmit(onSubmit)}>
                 <label className={formStyles.label}>First Name</label>
                 <input name="firstName" ref={register({ required: true })} />
-                {errors.firstName && <p>First name is required.</p>}
+                {errors.firstName && (
+                    <p className={formStyles.errorMessage}>
+                        First name is required.
+                    </p>
+                )}
 
                 <label className={formStyles.label}>Last Name</label>
                 <input name="lastName" ref={register({ required: true })} />
-                {errors.lastName && <p>Last name is required.</p>}
+                {errors.lastName && (
+                    <p className={formStyles.errorMessage}>
+                        Last name is required.
+                    </p>
+                )}
 
                 <label className={formStyles.label}>University</label>
-                <select name="university" ref={register({ required: true })}>
-                    <option value="" selected disabled hidden>
+                <select
+                    name="university"
+                    defaultValue="Select"
+                    ref={register({ required: true })}
+                >
+                    <option value="" selected>
                         Select
                     </option>
 
@@ -72,7 +98,11 @@ export default function GetInvolvedForm() {
                     <option value="Glasgow">Glasgow</option>
                     <option value="Edinburgh">Edinburgh</option>
                 </select>
-                {errors.university && <p>University is required.</p>}
+                {errors.university && (
+                    <p className={formStyles.errorMessage}>
+                        University is required.
+                    </p>
+                )}
 
                 <label className={formStyles.label}>Email Address</label>
                 <input
@@ -86,15 +116,24 @@ export default function GetInvolvedForm() {
                     })}
                 />
                 {errors.email && !errors.email.message && (
-                    <p>Email address is required.</p>
+                    <p className={formStyles.errorMessage}>
+                        Email address is required.
+                    </p>
                 )}
                 {errors.email && errors.email.message && (
-                    <p>Valid email address required.</p>
+                    <p className={formStyles.errorMessage}>
+                        Valid email address required.
+                    </p>
                 )}
                 <label className={formStyles.label}>Phone Number</label>
                 <input name="phoneNumber" ref={register({ required: true })} />
-                {errors.phoneNumber && <p>Phone number is required.</p>}
-                <input type="submit" />
+                {errors.phoneNumber && (
+                    <p className={formStyles.errorMessage}>
+                        Phone number is required.
+                    </p>
+                )}
+                <input type="submit" disabled={disabledButton} />
+                <p className={formStyles.message}>{message}</p>
             </form>
         </div>
     )
