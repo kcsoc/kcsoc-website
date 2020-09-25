d892be61be3f3f6d32d760cd60aaf7eaab284ea9
Josh Guha <joshguha@gmail.com>
2020-09-25 17:41:29 +0100

    Altered Card UI

--- src/components/events/eventCard.js
+++ src/components/events/eventCard.js
@@ -1,38 +1,44 @@
 import React from "react"
 import moment from "moment"
+import { FaFacebookF, FaInstagram } from "react-icons/fa"
 import eventCardStyles from "../../styles/components/events/eventCard.module.scss"
 
 export default function EventCard({ data }) {
-    const colorCard = () => {
-        if (data.type === "weekly") return eventCardStyles.weekly
-        if (data.type === "flagship") return eventCardStyles.flagship
-        if (data.type === "retreat") return eventCardStyles.retreat
-    }
-
     const date = moment(data.dateAndTime)
 
     return (
-        <div className={`${eventCardStyles.container} ${colorCard()}`}>
-            {data.poster && (
-                <img src={data.poster.resize.src} alt={data.poster.title} />
-            )}
+        <div className={eventCardStyles.card}>
+            <div className={eventCardStyles.image}>
+                {data.poster && (
+                    <img src={data.poster.resize.src} alt={data.poster.title} />
+                )}
+            </div>
             <div className={eventCardStyles.content}>
                 {data.name && (
-                    <p className={eventCardStyles.title}>{data.name}</p>
+                    <h2 className={eventCardStyles.title}>{data.name}</h2>
+                )}
+                {data.university && (
+                    <p className={eventCardStyles.university}>
+                        {data.university}
+                    </p>
+                )}
+
+                {data.dateAndTime && (
+                    <p>{date.format("Do MMMM YYYY - h:mm a")}</p>
                 )}
                 {data.speaker && <p>Speaker: {data.speaker}</p>}
-                {data.dateAndTime && (
-                    <p>Date: {date.format("Do MMMM YYYY")}</p>
-                )}
-                {data.dateAndTime && (
-                    <p>Time: {date.format("h:mm a")}</p>
-                )}
+
                 {data.location && <p>Location: {data.location}</p>}
+
+                <div className={eventCardStyles.icons}>
+                    <div className={eventCardStyles.facebook}>
+                        <FaFacebookF size="2em" />
+                    </div>
+                    <div className={eventCardStyles.instagram}>
+                        <FaInstagram size="2em" />
+                    </div>
+                </div>
             </div>
-            <p className={eventCardStyles.metaInfo}>
-                {data.type}
-                {data.university && ` - ${data.university}`}
-            </p>
         </div>
     )
 }
