d20a9994f49d6e84a9c2beddac4e1fd03bd9e02c
Josh Guha <joshguha@gmail.com>
2020-09-30 12:23:12 +0100

    Added Image loading message on Events page

--- src/pages/events.js
+++ src/pages/events.js
@@ -1,4 +1,5 @@
-import React from "react"
+import React, { useState } from "react"
+import BackgroundImageOnLoad from "background-image-on-load"
 import Header from "../components/header"
 import FadeIn from "../components/fadeIn"
 import Fade from "react-reveal/Fade"
@@ -6,7 +7,15 @@ import EventInfo from "../components/events/eventInfo"
 import EventCalendar from "../components/events/eventCalendar"
 
 export default function Events() {
-    return (
+    const [weeklyLoaded, setWeeklyLoaded] = useState(false)
+    const [flagshipLoaded, setFlagshipLoaded] = useState(false)
+    const [retreatsloaded, setRetreatsLoaded] = useState(false)
+
+    console.log(weeklyLoaded)
+
+    console.log(weeklyLoaded && flagshipLoaded && retreatsloaded)
+
+    return weeklyLoaded && flagshipLoaded && retreatsloaded ? (
         <FadeIn>
             <Header isOpaque={true} dark={true} />
 
@@ -17,6 +26,25 @@ export default function Events() {
                 <EventCalendar />
             </Fade>
         </FadeIn>
+    ) : (
+        <div>
+            <BackgroundImageOnLoad
+                src={"/weekly.png"}
+                onLoadBg={() => setWeeklyLoaded(true)}
+                onError={err => console.log("Error: ", err)}
+            />
+            <BackgroundImageOnLoad
+                src={"/flagship.jpg"}
+                onLoadBg={() => setFlagshipLoaded(true)}
+                onError={err => console.log("Error: ", err)}
+            />
+            <BackgroundImageOnLoad
+                src={"/retreats.png"}
+                onLoadBg={() => setRetreatsLoaded(true)}
+                onError={err => console.log("Error: ", err)}
+            />
+
+            <h1>Images are loading...</h1>
+        </div>
     )
 }
-
--- src/pages/explore.js
+++ src/pages/explore.js
@@ -1,10 +1,28 @@
-import React from "react"
-import Layout from '../components/layout'
+import React, { useState } from "react"
+import BackgroundImageOnLoad from "background-image-on-load"
 
 export default function Home() {
-  return (
-    <Layout>
-      <h1>Explore page</h1>
-    </Layout>
-  )
+    const [bgIsLoaded, setBgIsLoaded] = useState(false)
+    return (
+        <div
+            style={{
+                height: 1100,
+                backgroundPosition: "center",
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+
+                backgroundImage: `url(${
+                    !bgIsLoaded
+                        ? "https://picsum.photos/310/310?blur"
+                        : "https://picsum.photos/1600/310"
+                })`,
+            }}
+        >
+            <BackgroundImageOnLoad
+                src={"https://unsplash.it/1200/310?random"}
+                onLoadBg={() => setBgIsLoaded(true)}
+                onError={err => console.log("Error: ", err)}
+            />
+        </div>
+    )
 }
